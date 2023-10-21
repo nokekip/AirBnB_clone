a7f78b75c4c23456bfb3af4ba3ad5eacf981d4a9
nokekip <kipnoke@gmail.com>
2023-10-21 10:13:35 +0300

    removed typo

--- models/base_model.py
+++ models/base_model.py
@@ -48,7 +48,7 @@ class BaseModel:
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__"""
 
-      X  my_dict = self.__dict__.copy()
+        my_dict = self.__dict__.copy()
         my_dict["__class__"] = type(self).__name__
         my_dict["created_at"] = my_dict["created_at"].isoformat()
         my_dict["updated_at"] = my_dict["updated_at"].isoformat()
